67c80855edb7a4af7cb6546539977450a963c81a
slayeroak <roakley1@villanova.edu>
2019-12-30 18:29:55 -0500

    added a page title to index.html, create variables for game counts and set them to 0, created an array containing ABC's for computer letter guess...also added some commnets

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+  
+// Global Variables
+
+// alaphabet array for computer guesses
+var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+// Sets count variables for zero
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+var letterUser = [];
+var eachofLetters = null;
