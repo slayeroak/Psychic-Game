d692c144207db79f044555d43652c6053126f751
slayeroak <roakley1@villanova.edu>
2019-12-30 19:03:36 -0500

    added bootstrap links, added divs for container and row, made a global computerGuess variable equal to a random choice from the computerChoice array.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,3 +10,9 @@ var losses = 0;
 var guessesLeft = 9;
 var letterUser = [];
 var eachofLetters = null;
+
+// Sets the computerGuess variable equal to a random choice from the computerChoice array.
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+
+// GAME FUNCTIONS
